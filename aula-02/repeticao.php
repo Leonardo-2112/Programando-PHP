b4a89f7b0cbd9683864f68b5fc610ffa10706174
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-04-08 17:22:33 -0300

    Adicionando o arquivo repeticao (aula-02)

---
+++ aula-02/repeticao.php
@@ -0,0 +1,11 @@
+<?php
+//for (início; condição para repetição (fim); incremento)
+// for($i = 1; $i <= 5; $i++){
+//     echo($i);
+// }
+for($i = 1; $i <= 10; $i++);{
+    if($i == 5){
+        break;
+    }
+    echo $i . "<br>";
+}
