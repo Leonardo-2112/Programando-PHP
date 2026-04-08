913148dd8b7370b3c9d0ae4a1aaa51626028b80f
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-04-08 16:41:12 -0300

    Adicionando arquivo array (aula-02)

---
+++ aula-02/array.php
@@ -0,0 +1,18 @@
+<?php
+//array
+
+// $nome1 = "João";
+// $nome2 = "Maria";
+// $nome3 = "Pedro";
+
+// $nomes = ["João" , "Maria" , "Pedro", "Leonardo", "Wilton"];
+// $recebe = $nomes[3];
+// echo $recebe;
+
+
+$frutas = array("Maçã", "Laranja", "Banana");
+// print_r ($frutas); print_r mostra todas as posições do array
+// var_dump($frutas); var_dump mostra todas as posiçoes, o tipo e a quantidade de caracteres do array
+
+$cestaFrutas = $frutas;// da valor a variável a partir do array
+print_r($cestaFrutas);
