5453b89f5fe470226abbd84984fe7808733a9280
Leonardo-2112 <afoncoleonardo21@gmail.com>
2026-04-08 16:56:47 -0300

    Adicionando arquivo atividade_array (aula-02)

--- aula-02/array.php
+++ aula-02/array.php
@@ -14,5 +14,11 @@ $frutas = array("Maçã", "Laranja", "Banana");
 // print_r ($frutas); print_r mostra todas as posições do array
 // var_dump($frutas); var_dump mostra todas as posiçoes, o tipo e a quantidade de caracteres do array
 
-$cestaFrutas = $frutas;// da valor a variável a partir do array
-print_r($cestaFrutas);
+// $cestaFrutas = $frutas;          //Da valor a variável a partir do array
+// print_r($cestaFrutas);
+
+// array_unshift($frutas, "Uva");   //Adiciona um valor no ínicio do array
+// print_r($frutas);
+
+// array_push($frutas, "Tomate");   //Adiciona valor no final do array
+// print_r($frutas);
---
+++ aula-02/atividade_array.php
@@ -0,0 +1,3 @@
+<?php
+$alunos = ["Guilherme", "Gabriel", "Vinicius.S", "Kauã", "Ryan", "Guilherme.S", "Diogo", "Vinicios.M", "Júlia", "Nicolas", "Guilherme.F"];
+print_r($alunos[0]);
